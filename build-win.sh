f2fc5468f8c534a64f32f303c42d5da843a9daff
Alex Greenall <bbtsr@foxmail.com>
2020-11-18 00:51:52 +0800

    adapt windows(cygwin) build

--- build-win.sh
+++ build-win.sh
@@ -8,7 +8,7 @@ if [ "$?" == "0" ]; then
     -Wno-format-nonliteral \
     -Wno-cast-qual \
     -Wno-padded \
-    -fPIC -shared -ldl -o libpl2.so
+    -fPIC -shared -ldl -o libpl2.dll
   clang main.c \
     -Weverything \
     -Wno-zero-length-array \
@@ -19,17 +19,17 @@ if [ "$?" == "0" ]; then
     -Wno-zero-length-array \
     -Wno-padded \
     -L. -lpl2 -o pl2
-    -fPIC -shared -o libpldbg.so
+    -fPIC -shared -o libpldbg.dll
   clang echo.c \
     -Weverything \
     -Wno-zero-length-array \
     -Wno-padded \
     -L. -lpl2 -o pl2
-    -fPIC -shared -o libecho.so
+    -fPIC -shared -o libecho.dll
 else
-  cc pl2.c -Wall -Wextra -fPIC -shared -ldl -o libpl2.so
+  cc pl2.c -Wall -Wextra -fPIC -shared -ldl -o libpl2.dll
   cc main.c -Wall -Wextra -L. -lpl2 -o pl2
-  cc pldbg.c -Wall -Wextra -fPIC -shared -L. -lpl2  -o libpldbg.so
-  cc echo.c -Wall -Wextra -fPIC -shared -L. -lpl2 -o libecho.so
+  cc pldbg.c -Wall -Wextra -fPIC -shared -L. -lpl2  -o libpldbg.dll
+  cc echo.c -Wall -Wextra -fPIC -shared -L. -lpl2 -o libecho.dll
 fi
 
--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 rm -f *.o
 rm -f *.so
+rm -f *.dll
 rm -f pl2
 rm -f *.exe
 rm -f *.gch
